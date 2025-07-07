2aaa0ac1e4c7491634cf107aa920deef951f3c18
tosuki <okaabe2006@gmail.com>
2025-07-06 21:35:18 -0300

    feat: add less to scoop packages

--- reinstall_scoop_packages.bat
+++ reinstall_scoop_packages.bat
@@ -12,5 +12,5 @@ scoop bucket add versions
 scoop bucket add java
 
 echo Installing packages...
-scoop install 7zip 7zip19.00-helper docker docker-compose fastfetch fzf gimp googlechrome gradle innounp komorebi maven3 openjdk17 postgresql qutebrowser sharex vim vscode whkd winrar
+scoop install 7zip 7zip19.00-helper docker docker-compose fastfetch fzf gimp googlechrome gradle innounp komorebi maven3 openjdk17 postgresql qutebrowser sharex vim vscode whkd winrar less
 winget install -e --id ZeroTier.ZeroTier-One 
