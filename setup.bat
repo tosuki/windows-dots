a93d4af3277913d2ca717910298c0dfcb49b603d
tosuki <okaabe2006@gmail.com>
2025-07-06 21:18:59 -0300

    feat: add komorebic initialization after the installation

--- setup.bat
+++ setup.bat
@@ -7,5 +7,9 @@ copy /Y .vimrc %USERPROFILE%\.vimrc
 copy /Y komorebi.json %USERPROFILE%\komorebi.json
 xcopy /E /I /Y .config %USERPROFILE%\.config
 copy /Y terminal\settings.json %LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
-echo.
+echo Enabling Komorebi autostart
+komorebic enable-autostart --whkd  
+echo Initializing Komorebi
+komorebic start 
 echo Script finished.
+
